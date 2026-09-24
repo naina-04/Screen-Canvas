41328c07fab4c3099f9d0553e3f7996a04681f61
naina-04 <badduriharshitha@gmail.com>
2026-09-24 17:17:06 +0530

    feat(geometry): implement ray-casting point-in-polygon collision detection

---
+++ src/shared/utils/polygonCollision.ts
@@ -0,0 +1,20 @@
+import { Point } from '../types';
+
+export function isPointInsidePolygon(point: Point, polygon: Point[]): boolean {
+  if (polygon.length < 3) return false;
+
+  let inside = false;
+  const { x, y } = point;
+
+  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    const xi = polygon[i].x;
+    const yi = polygon[i].y;
+    const xj = polygon[j].x;
+    const yj = polygon[j].y;
+
+    const intersect = yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
+}
