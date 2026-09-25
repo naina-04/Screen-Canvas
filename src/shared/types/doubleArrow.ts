cb95ef55c5c285daa241f29576f29e58284559c9
naina-04 <badduriharshitha@gmail.com>
2026-09-25 11:38:10 +0530

    feat(geometry): define double-ended arrow dimensions in src/shared/types/doubleArrow.ts

---
+++ src/shared/types/doubleArrow.ts
@@ -0,0 +1,10 @@
+import { Point } from './index';
+
+export interface DoubleArrowGeometry {
+  startTip: Point;
+  startLeft: Point;
+  startRight: Point;
+  endTip: Point;
+  endLeft: Point;
+  endRight: Point;
+}
